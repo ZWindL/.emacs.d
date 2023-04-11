61158e9b1d319a55fecf39a95c80f2d335c809f4
zwindl <zwindl@protonmail.com>
2023-04-11 15:10:16 +0800

    Switch to built-in tree-sitter mode

--- settings/init-tree-sitter.el
+++ settings/init-tree-sitter.el
@@ -7,22 +7,20 @@
 
 ;; Tree-sitter
 (use-package tree-sitter
-  :ensure t
   :hook
-  (prog-mode . global-tree-sitter-mode)
-  (tree-sitter-after-on . tree-sitter-hl-mode)
-  :config
-  (use-package tree-sitter-langs
-    :ensure t))
+  (prog-mode            . global-tree-sitter-mode)
+  (tree-sitter-after-on . tree-sitter-hl-mode))
 
-;; TODO: ⛔ Error (use-package): treesit-auto/:config: Symbol’s function definition is void: global-treesit-auto-mode
-;; (use-package treesit-auto
-;;   :ensure t
-;;   :demand t
-;;   :config
-;;   (add-to-list 'treesit-auto-fallback-alist '(bash-ts-mode . sh-mode))
-;;   (setq treesit-auto-install 'prompt)
-;;   (global-treesit-auto-mode))
+(use-package tree-sitter-langs :ensure t)
+
+(use-package treesit-auto
+  :ensure t
+  :demand t
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  ;; (add-to-list 'treesit-auto-fallback-alist '(bash-ts-mode . sh-mode))
+  (global-treesit-auto-mode))
 
 (provide 'init-tree-sitter)
 ;;; init-tree-sitter.el ends here
