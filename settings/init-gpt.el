798b61b816394446cc7214a4436ebda4d41da11c
zwindl <zwindl@protonmail.com>
2023-04-11 20:05:51 +0800

    fix a bug in gpt api key fetching function

--- settings/init-gpt.el
+++ settings/init-gpt.el
@@ -11,12 +11,13 @@
   (gptel-api-key (get-gpt-api-key))
   :init
   (defun get-gpt-api-key ()
-    "Read the contents of the `gpt.api.key' file in the Emacs user config root directory and return its contents as a string."
+    "Read the contents of the `gpt.api.key' file in the Emacs user config root."
     (let ((key-file (expand-file-name "gpt.api.key" user-emacs-directory)))
       (when (file-exists-p key-file)
         (with-temp-buffer
           (insert-file-contents key-file)
-          (buffer-substring-no-properties (point-min) (point-max)))))))
+          (string-trim-right (buffer-string)))))))
+
 
 (provide 'init-gpt)
 ;;; init-gpt.el ends here
